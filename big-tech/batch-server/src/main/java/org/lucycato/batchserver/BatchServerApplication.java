edaeed53aa82094bbcbfa6f02f80a22c39512dea
will_d <dev_willd@naver.com>
2024-02-28 21:06:18 +0900

    application init

---
+++ big-tech/batch-server/src/main/java/org/lucycato/batchserver/BatchServerApplication.java
@@ -0,0 +1,11 @@
+package org.lucycato.batchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BatchServerApplication.class, args);
+    }
+}
