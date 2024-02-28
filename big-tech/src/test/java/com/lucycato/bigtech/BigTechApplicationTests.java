52d362461a044ade83880cc3def1e46652d7715e
will_d <dev_willd@naver.com>
2024-02-28 19:23:56 +0900

    project init

---
+++ big-tech/src/test/java/com/lucycato/bigtech/BigTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucycato.bigtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
