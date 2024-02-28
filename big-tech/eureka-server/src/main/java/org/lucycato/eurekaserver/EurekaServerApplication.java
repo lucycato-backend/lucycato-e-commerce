3962d48e2ff11476c23eb512b9aa1133727c0583
will_d <dev_willd@naver.com>
2024-02-28 21:06:18 +0900

    LUCY-004: application init

---
+++ big-tech/eureka-server/src/main/java/org/lucycato/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,11 @@
+package org.lucycato.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
---
+++ big-tech/member-service/src/main/java/org/lucycato/memberservice/MemberServiceApplication.java
@@ -0,0 +1,11 @@
+package org.lucycato.memberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberServiceApplication.class, args);
+    }
+}
