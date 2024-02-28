ecd4cc1bac32f432016b41f9674fd1e5f93f7dcd
will_d <dev_willd@naver.com>
2024-02-28 21:06:59 +0900

    application init

---
+++ big-tech/delivery-service/src/main/java/org/lucycato/deliveryservice/DeliveryServiceApplication.java
@@ -0,0 +1,11 @@
+package org.lucycato.deliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryServiceApplication.class, args);
+    }
+}
