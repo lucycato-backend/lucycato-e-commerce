58a47aa134b663d3947fa2a842dcac599fa74092
will_d <dev_willd@naver.com>
2024-02-29 19:24:26 +0900

    LUCY-005: SelfValidating 구축

---
+++ big-tech/common/src/main/java/org/lucycato/common/SelfValidating.java
@@ -0,0 +1,21 @@
+package org.lucycato.common;
+
+import jakarta.validation.*;
+
+import java.util.Set;
+
+public class SelfValidating<T> {
+    private final Validator validator;
+
+    public SelfValidating() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    protected void validateSelf() {
+        Set<ConstraintViolation<T>> violations = validator.validate((T) this);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
+    }
+}
