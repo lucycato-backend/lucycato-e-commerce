67b797d60a6940e7e1a202200dabe3cce4171c69
will_d <dev_willd@naver.com>
2024-02-29 20:14:22 +0900

    [FEAT] KafkaResultTaskConsumerRegisterHelper 구현

---
+++ big-tech/common/src/main/java/org/lucycato/common/KafkaResultTaskConsumerRegisterHelper.java
@@ -0,0 +1,34 @@
+package org.lucycato.common;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Getter
+@Component
+public class KafkaResultTaskConsumerRegisterHelper {
+    private final String bootstrapServers;
+
+    @Getter
+    private final String topic;
+
+    public KafkaResultTaskConsumerRegisterHelper(
+            @Value("${kafka.clusters.bootstrapservers:null}") String bootstrapServers,
+            @Value("${task.result.topic:null}") String topic
+    ) {
+        this.bootstrapServers = bootstrapServers;
+        this.topic = topic;
+    }
+
+    public Properties createPropsConsumerTaskResultTopic(String groupId) {
+        if (bootstrapServers == null || topic == null) throw new RuntimeException("server environment is not exist please setting environment");
+        Properties props = new Properties();
+        props.put("bootstrap.servers", bootstrapServers);
+        props.put("group.id", groupId);
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        return props;
+    }
+}
