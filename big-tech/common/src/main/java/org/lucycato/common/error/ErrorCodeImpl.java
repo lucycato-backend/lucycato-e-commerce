dae046ef1baf54f46784b40c172a68139329ef82
learnGrowD <dev_willd@naver.com>
2024-03-02 00:04:01 +0900

    LUSY-003: web client error code 추가

--- big-tech/common/src/main/java/org/lucycato/common/error/ErrorCodeImpl.java
+++ big-tech/common/src/main/java/org/lucycato/common/error/ErrorCodeImpl.java
@@ -10,10 +10,11 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCodeImpl implements ErrorCode {
     INTERNAL_SERVER(HttpStatus.INTERNAL_SERVER_ERROR.value(), "LC-0001", "internal server error", ""),
     BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), "LC-0002", "bad request", ""),
-    CLIENT(413, "LC-0003", "request server error reason", ""),
-    NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR.value(), "LC-0004", "null point", ""),
-    VALIDATION(HttpStatus.INTERNAL_SERVER_ERROR.value(), "LC-0005", "fail validation", ""),
-    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED.value(), "LC-0006", "method not allowed please method change", "")
+    REQUEST_CLIENT(HttpStatus.INTERNAL_SERVER_ERROR.value(), "LC-0003", "client request server error", ""),
+    RESPONSE_CLIENT(HttpStatus.INTERNAL_SERVER_ERROR.value(), "LC-0004", "client response error reason", ""),
+    NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR.value(), "LC-0005", "null point", ""),
+    VALIDATION(HttpStatus.INTERNAL_SERVER_ERROR.value(), "LC-0006", "fail validation", ""),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED.value(), "LC-0007", "method not allowed please method change", "")
     ;
     private final Integer httpCode;
     private final String code;
