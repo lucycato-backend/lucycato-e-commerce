d53278829f0cf4d27c68d79694dcf98f191aba89
learnGrowD <dev_willd@naver.com>
2024-03-01 17:32:12 +0900

    LUCY-002: validation, lombok 의존성 추가 및 common component bean 등록 config 추가

---
+++ big-tech/member-service/src/main/java/org/lucycato/memberservice/MemberServiceConfig.java
@@ -0,0 +1,9 @@
+package org.lucycato.memberservice;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.lucycato.common")
+public class MemberServiceConfig {
+}
