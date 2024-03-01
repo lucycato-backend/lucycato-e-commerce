0fb470d14f96b1b37adb5a2877a7e9f8d89cf9c4
learnGrowD <dev_willd@naver.com>
2024-03-01 17:04:15 +0900

    [FEAT] exception handler 개발

---
+++ big-tech/common/src/main/java/org/lucycato/common/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package org.lucycato.common.exceptionhandler;
+
+import lombok.RequiredArgsConstructor;
+import org.lucycato.common.LoggingProducer;
+import org.lucycato.common.PrintStackTraceManager;
+import org.lucycato.common.api.Api;
+import org.lucycato.common.error.ErrorCode;
+import org.lucycato.common.error.ErrorCodeImpl;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import org.springframework.web.server.MethodNotAllowedException;
+import org.springframework.web.server.ServerWebInputException;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+@Order(Integer.MAX_VALUE)
+@ControllerAdvice
+@RequiredArgsConstructor
+public class GlobalExceptionHandler {
+    private final LoggingProducer loggingProducer;
+    private final PrintStackTraceManager printStackTraceManager;
+
+    @ExceptionHandler(value = Exception.class)
+    public Mono<ResponseEntity<Api<Object>>> handleGlobalException(Exception ex) {
+        ex.printStackTrace();
+        String stackTranceString = printStackTraceManager.getStackTraceAsString(ex);
+        loggingProducer.sendLogMessage("exception", stackTranceString);
+
+        return validateException(ex)
+                .map(tuple -> ResponseEntity
+                        .status(tuple.getT1().getHttpCode())
+                        .body(Api.ERROR(tuple.getT1(), tuple.getT2()))
+                );
+    }
+
+    private Mono<Tuple2<ErrorCode, String>> validateException(Exception ex) {
+        ErrorCode errorCode;
+        String reason;
+        //TODO: validation exception message 고도화
+        if (ex instanceof ServerWebInputException) {
+            errorCode = ErrorCodeImpl.BAD_REQUEST;
+            reason = errorCode.getReason() + ": " + ((ServerWebInputException) ex).getReason();
+        } else if (ex instanceof MethodNotAllowedException) {
+            errorCode = ErrorCodeImpl.METHOD_NOT_ALLOWED;
+            reason = errorCode.getReason();
+        } else {
+            errorCode = ErrorCodeImpl.INTERNAL_SERVER;
+            reason = errorCode.getReason();
+        }
+        return Mono.just(Tuples.of(errorCode, reason));
+    }
+}
