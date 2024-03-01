b076fa821db0fc94a2b617b1a827ae3b49de5042
learnGrowD <dev_willd@naver.com>
2024-03-01 17:03:13 +0900

    LUCY-002: error code 고도화

--- big-tech/common/src/main/java/org/lucycato/common/error/ErrorCodeImpl.java
+++ big-tech/common/src/main/java/org/lucycato/common/error/ErrorCodeImpl.java
@@ -2,14 +2,26 @@ package org.lucycato.common.error;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.lucycato.common.exception.ApiExceptionImpl;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCodeImpl implements ErrorCode {
-    OK(HttpStatus.OK.value(), "0000", "Success"),
+    INTERNAL_SERVER(HttpStatus.INTERNAL_SERVER_ERROR.value(), "LC-0001", "internal server error", ""),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), "LC-0002", "bad request", ""),
+    CLIENT(413, "LC-0003", "request server error reason", ""),
+    NULL_POINT(HttpStatus.INTERNAL_SERVER_ERROR.value(), "LC-0004", "null point", ""),
+    VALIDATION(HttpStatus.INTERNAL_SERVER_ERROR.value(), "LC-0005", "fail validation", ""),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED.value(), "LC-0006", "method not allowed please method change", "")
     ;
     private final Integer httpCode;
     private final String code;
     private final String reason;
+    private final String frontMessage;
+
+    @Override
+    public RuntimeException build(Object ...args) {
+        return new ApiExceptionImpl(this, reason.formatted(args));
+    }
 }
