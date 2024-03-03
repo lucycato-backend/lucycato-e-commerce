1155ac944bb56423a0e318d594ea74a323acb97e
learnGrowD <dev_willd@naver.com>
2024-03-03 19:28:49 +0900

    LUSY-007: logging common component bean 등록

---
+++ big-tech/logging-consumer/src/main/java/org/lucycato/loggingconsumer/LoggingConsumerConfig.java
@@ -0,0 +1,9 @@
+package org.lucycato.loggingconsumer;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.lucycato.common")
+public class LoggingConsumerConfig {
+}
