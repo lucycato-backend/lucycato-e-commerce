74a726fe2a9f25c73bc74beba4c325f551621796
learnGrowD <dev_willd@naver.com>
2024-03-03 23:34:15 +0900

    LUSY-007: task-consumer save history task domain 개발

---
+++ big-tech/task-consumer/src/main/java/org/lucycato/taskconsumer/domain/SaveTaskHistoryR2dbcEntity.java
@@ -0,0 +1,41 @@
+package org.lucycato.taskconsumer.domain;
+
+import lombok.*;
+import org.lucycato.common.model.task.TaskKeyCategory;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@Table(name = "save_task_history")
+@Setter
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class SaveTaskHistoryR2dbcEntity {
+    @Id
+    private Long id;
+
+    private String taskKeyCategory;
+
+    private String taskHistoryStatus;
+
+    private String transactionUUID;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    public static SaveTaskHistoryR2dbcEntity register(
+            TaskKeyCategory taskKeyCategory,
+            SaveTaskHistoryStatus taskHistoryStatus,
+            String transactionUUID
+    ) {
+        return SaveTaskHistoryR2dbcEntity.builder()
+                .taskKeyCategory(taskKeyCategory.name())
+                .taskHistoryStatus(taskHistoryStatus.name())
+                .transactionUUID(transactionUUID)
+                .build();
+    }
+}
