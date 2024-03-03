39ebd39cf0f31856ef51f49836e93a05dd11b2b3
learnGrowD <dev_willd@naver.com>
2024-03-03 23:25:25 +0900

    LUCY-007: stream function 수정

--- big-tech/logging-consumer/src/main/java/org/lucycato/loggingconsumer/application/port/in/LoggingUseCase.java
+++ big-tech/logging-consumer/src/main/java/org/lucycato/loggingconsumer/application/port/in/LoggingUseCase.java
@@ -3,5 +3,5 @@ package org.lucycato.loggingconsumer.application.port.in;
 import reactor.core.publisher.Mono;
 
 public interface LoggingUseCase {
-    void handleLogging(HandleLoggingCommand command);
+    Mono<Void> handleLogging(HandleLoggingCommand command);
 }
