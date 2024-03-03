c868b030e55c46f1fabc88a58745c947b3c775f0
learnGrowD <dev_willd@naver.com>
2024-03-03 23:34:30 +0900

    LUSY-007: task-consumer out port 개발

---
+++ big-tech/task-consumer/src/main/java/org/lucycato/taskconsumer/application/port/out/SaveTaskHistoryPort.java
@@ -0,0 +1,12 @@
+package org.lucycato.taskconsumer.application.port.out;
+
+import org.lucycato.common.model.task.TaskKey;
+import org.lucycato.taskconsumer.domain.SaveTaskHistoryR2dbcEntity;
+import org.lucycato.taskconsumer.domain.SaveTaskHistoryStatus;
+import reactor.core.publisher.Mono;
+
+public interface SaveTaskHistoryPort {
+    Mono<SaveTaskHistoryR2dbcEntity> createSaveTaskHistory(TaskKey taskKey, SaveTaskHistoryStatus taskHistoryStatus);
+
+    Mono<SaveTaskHistoryR2dbcEntity> modifySaveTaskHistoryStatus(SaveTaskHistoryR2dbcEntity entity, SaveTaskHistoryStatus taskHistoryStatus);
+}
