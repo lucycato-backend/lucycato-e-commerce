88ce48089d0868876483deb1403d5a0805b79ff6
learnGrowD <dev_willd@naver.com>
2024-03-03 23:25:19 +0900

    LUCY-007: stream function 수정

--- big-tech/logging-consumer/src/main/java/org/lucycato/loggingconsumer/application/service/LoggingService.java
+++ big-tech/logging-consumer/src/main/java/org/lucycato/loggingconsumer/application/service/LoggingService.java
@@ -9,7 +9,8 @@ import reactor.core.publisher.Mono;
 public class LoggingService implements LoggingUseCase {
 
     @Override
-    public void handleLogging(HandleLoggingCommand command) {
+    public Mono<Void> handleLogging(HandleLoggingCommand command) {
         System.out.println("[Logging Consumer] Received logging message -> " + " < " + command.getLoggingKey() + " > " + command.getLoggingValue());
+        return Mono.empty();
     }
 }
