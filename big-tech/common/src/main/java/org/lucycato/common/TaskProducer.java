805348a2e744e667c4b10249e05f1c332fdad7ae
learnGrowD <dev_willd@naver.com>
2024-03-03 23:26:52 +0900

    LUCY-007: sendTask logic 보완

--- big-tech/common/src/main/java/org/lucycato/common/TaskProducer.java
+++ big-tech/common/src/main/java/org/lucycato/common/TaskProducer.java
@@ -1,12 +1,17 @@
 package org.lucycato.common;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.lucycato.common.annotation.out.ProducerAdapter;
+import org.lucycato.common.error.ErrorCodeImpl;
+import org.lucycato.common.model.task.TaskKey;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 import java.util.Properties;
 
@@ -39,10 +44,11 @@ public class TaskProducer {
         this.printStackTraceManager = printStackTraceManager;
     }
 
-    public Mono<Void> sendTask(String taskKey, Object taskValue) {
-        if (bootstrapServers == null || topic == null) return Mono.error(new RuntimeException(""));
-        return Mono.fromCallable(() -> objectMapper.writeValueAsString(taskValue))
-                .map(taskValueJsonString -> new ProducerRecord<>(topic, taskKey, taskValueJsonString))
+    public Mono<Void> sendTask(TaskKey taskKey, Object taskValue) {
+        if (bootstrapServers == null || topic == null) return Mono.error(ErrorCodeImpl.KAFKA_SEND_TASK_FAIL.build());
+        return Mono.fromCallable(() -> Tuples.of(objectMapper.writeValueAsString(taskKey), objectMapper.writeValueAsString(taskValue)))
+                .onErrorResume(JsonProcessingException.class, error -> Mono.error(ErrorCodeImpl.JSON_PARSING.build()))
+                .map(tuple -> new ProducerRecord<>(topic, tuple.getT1(), tuple.getT2()))
                 .flatMap(record -> Mono.create(sink -> kafkaProducer.send(record, (metadata, exception) -> {
                     if (exception != null) {
                         exception.printStackTrace();
