c44b793e0a79a5aba8c2b3f4fe6c1b5f2e1f57fc
learnGrowD <dev_willd@naver.com>
2024-03-03 14:40:05 +0900

    LUSY-007: logging-consumer in port 추가

---
+++ big-tech/logging-consumer/src/main/java/org/lucycato/loggingconsumer/application/port/in/HandleLoggingCommand.java
@@ -0,0 +1,11 @@
+package org.lucycato.loggingconsumer.application.port.in;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HandleLoggingCommand {
+    private String loggingKey;
+    private String loggingValue;
+}
