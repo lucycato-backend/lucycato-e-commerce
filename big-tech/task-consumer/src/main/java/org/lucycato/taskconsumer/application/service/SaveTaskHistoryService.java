047a67fc43ce1bf2e546eb25152904b27cb17033
learnGrowD <dev_willd@naver.com>
2024-03-04 23:04:32 +0900

    LUCY-002: json parsing logic service -> consumer 분리

--- big-tech/task-consumer/src/main/java/org/lucycato/taskconsumer/application/service/SaveTaskHistoryService.java
+++ big-tech/task-consumer/src/main/java/org/lucycato/taskconsumer/application/service/SaveTaskHistoryService.java
@@ -1,11 +1,7 @@
 package org.lucycato.taskconsumer.application.service;
 
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.lucycato.common.annotation.in.UseCase;
-import org.lucycato.common.error.ErrorCodeImpl;
-import org.lucycato.common.model.task.TaskKey;
 import org.lucycato.taskconsumer.application.port.in.SaveTaskHistoryAndSendTaskResultCommand;
 import org.lucycato.taskconsumer.application.port.in.SaveTaskHistoryUseCase;
 import org.lucycato.taskconsumer.application.port.out.SaveTaskHistoryPort;
@@ -17,7 +13,6 @@ import reactor.core.publisher.Mono;
 @UseCase
 @RequiredArgsConstructor
 public class SaveTaskHistoryService implements SaveTaskHistoryUseCase {
-    private final ObjectMapper objectMapper;
 
     private final SendResultTaskPort sendResultTaskPort;
 
@@ -25,12 +20,11 @@ public class SaveTaskHistoryService implements SaveTaskHistoryUseCase {
 
     @Override
     public Mono<SaveTaskHistoryR2dbcEntity> saveTaskHistoryAndSendTaskResult(SaveTaskHistoryAndSendTaskResultCommand command) {
-        return Mono.fromCallable(() -> objectMapper.readValue(command.getTaskKeyJsonString(), TaskKey.class))
-                .onErrorResume(JsonMappingException.class, error -> Mono.error(ErrorCodeImpl.JSON_PARSING.build()))
-                .flatMap(taskKey -> saveTaskHistoryPort.createSaveTaskHistory(taskKey, SaveTaskHistoryStatus.REQUEST)
-                        .flatMap(entity -> sendResultTaskPort.sendTaskResultJsonMessage(taskKey, command.getValueJsonString())
+        return saveTaskHistoryPort.createSaveTaskHistory(command.getTaskKey(), SaveTaskHistoryStatus.REQUEST)
+                .flatMap(entity ->
+                        sendResultTaskPort.sendTaskResultJsonMessage(command.getTaskKey(), command.getValueJsonString())
                                 .then(saveTaskHistoryPort.modifySaveTaskHistoryStatus(entity, SaveTaskHistoryStatus.SUCCESS))
                                 .onErrorResume(error -> saveTaskHistoryPort.modifySaveTaskHistoryStatus(entity, SaveTaskHistoryStatus.FAIL))
-                        ));
+                );
     }
 }
