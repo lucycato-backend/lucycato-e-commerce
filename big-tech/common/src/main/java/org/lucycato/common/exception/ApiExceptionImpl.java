f9250dc8c7892dbe046c587688c6d8bedaef3657
learnGrowD <dev_willd@naver.com>
2024-03-06 00:05:12 +0900

    [FIX] ApiException constructor 설계 및 반영

--- big-tech/common/src/main/java/org/lucycato/common/exception/ApiExceptionImpl.java
+++ big-tech/common/src/main/java/org/lucycato/common/exception/ApiExceptionImpl.java
@@ -2,11 +2,37 @@ package org.lucycato.common.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.lucycato.common.api.Result;
 import org.lucycato.common.error.ErrorCode;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class ApiExceptionImpl extends RuntimeException implements ApiException {
-    private ErrorCode errorCode;
-    private String reason;
+    private Integer httpCode;
+    private Result result;
+
+    public ApiExceptionImpl(Integer httpCode, Result result) {
+        super(result.getReason());
+        this.httpCode = httpCode;
+        this.result = result;
+    }
+
+    public ApiExceptionImpl(ErrorCode errorCode) {
+        super(errorCode.getReason());
+        this.httpCode = errorCode.getHttpCode();
+        this.result = Result.ERROR(errorCode);
+    }
+
+    public ApiExceptionImpl(ErrorCode errorCode, String reason) {
+        super(reason);
+        this.httpCode = errorCode.getHttpCode();
+        this.result = Result.ERROR(errorCode, reason);
+    }
+
+    public ApiExceptionImpl(ErrorCode errorCode, Throwable tx) {
+        super(tx.getLocalizedMessage());
+        this.httpCode = errorCode.getHttpCode();
+        this.result = Result.ERROR(errorCode);
+    }
 }
