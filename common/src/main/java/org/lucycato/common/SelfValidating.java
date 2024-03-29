f23a1113aea18a556ded56b7cdd2f397a31f6030
learnGrowD <dev_willd@naver.com>
2024-03-29 21:36:02 +0900

    LUCY-0010: common self validation init exception 삭제

--- common/src/main/java/org/lucycato/common/SelfValidating.java
+++ common/src/main/java/org/lucycato/common/SelfValidating.java
@@ -1,6 +1,9 @@
 package org.lucycato.common;
 
-import jakarta.validation.*;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import org.lucycato.common.error.ErrorCodeImpl;
 import org.lucycato.common.exception.ApiExceptionImpl;
 
@@ -14,7 +17,7 @@ public abstract class SelfValidating<T> {
         validator = factory.getValidator();
     }
 
-    protected void validateSelf() throws Exception {
+    protected void validateSelf() {
         Set<ConstraintViolation<T>> violations = validator.validate((T) this);
         if (!violations.isEmpty()) {
             throw new ApiExceptionImpl(ErrorCodeImpl.VALIDATION);
