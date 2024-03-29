95d69274847c4a06dfc5adfbce006fa1da435478
learnGrowD <dev_willd@naver.com>
2024-03-29 21:35:45 +0900

    LUCY-0010: common annotation package 구조 수정

--- common/src/main/java/org/lucycato/common/annotation/hexagonal/in/ConsumerAdapter.java
+++ common/src/main/java/org/lucycato/common/annotation/hexagonal/in/ConsumerAdapter.java
@@ -1,4 +1,4 @@
-package org.lucycato.common.annotation.out;
+package org.lucycato.common.annotation.hexagonal.in;
 
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Component;
@@ -9,7 +9,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Component
-public @interface ExternalSystemAdapter {
+public @interface ConsumerAdapter {
     @AliasFor(annotation = Component.class)
     String value() default "";
 }
