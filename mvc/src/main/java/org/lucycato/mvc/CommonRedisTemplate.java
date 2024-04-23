993801b071d5697592c9dc8af56c583d5f2325e8
learnGrowD <dev_willd@naver.com>
2024-04-23 19:28:33 +0900

    feat/LUCY-016: common redis template findAll 추가

--- mvc/src/main/java/org/lucycato/mvc/CommonRedisTemplate.java
+++ mvc/src/main/java/org/lucycato/mvc/CommonRedisTemplate.java
@@ -3,8 +3,13 @@ package org.lucycato.mvc;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.lucycato.common.error.ErrorCodeImpl;
+import org.lucycato.common.exception.ApiExceptionImpl;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -13,19 +18,32 @@ public class CommonRedisTemplate {
 
     private final ObjectMapper objectMapper;
 
-    public <T> T save(String key, T value) throws Exception {
+    public <T> T save(String key, T value, Long ttl, TimeUnit timeUnit) throws Exception {
         String jsonString = objectMapper.writeValueAsString(value);
-        redisTemplate.opsForValue().set(key, jsonString);
+        redisTemplate.opsForValue().set(key, jsonString, ttl, timeUnit);
         return value;
     }
 
-    public <T> T find(String key, String value) throws Exception {
+    public <T> T find(String key) throws Exception {
         String jsonString = redisTemplate.opsForValue().get(key);
-        return objectMapper.readValue(jsonString, new TypeReference<T>() {});
+        return objectMapper.readValue(jsonString, new TypeReference<T>() {
+        });
     }
 
-    public <T> T update(String key, T value) throws Exception {
-        return save(key, value);
+    public <T> List<T> findAll(List<String> keys) throws Exception {
+        List<String> jsonStringList = redisTemplate.opsForValue().multiGet(keys);
+        return jsonStringList.stream().map(jsonString -> {
+            try {
+                return objectMapper.readValue(jsonString, new TypeReference<T>() {
+                });
+            } catch (Exception e) {
+                throw new ApiExceptionImpl(ErrorCodeImpl.JSON_PARSING);
+            }
+        }).toList();
+    }
+
+    public <T> T update(String key, T value, Long ttl, TimeUnit timeUnit) throws Exception {
+        return save(key, value, ttl, timeUnit);
     }
 
     public void delete(String key) {
