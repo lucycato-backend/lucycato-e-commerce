7c524bf41ebc6bdc96d014de77e31505f71f014c
learnGrowD <dev_willd@naver.com>
2024-05-14 13:01:11 +0900

    feat/LUCY-027: WebFlux Filter PublishOn, SubscribeOn add

--- webflux/src/main/java/org/lucycato/webflux/filter/WebFluxFilter.java
+++ webflux/src/main/java/org/lucycato/webflux/filter/WebFluxFilter.java
@@ -6,6 +6,7 @@ import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
 
@@ -16,11 +17,16 @@ public class WebFluxFilter implements WebFilter {
         List<String> adminOrAppMemberJsonStringList = exchange.getRequest().getHeaders().get(XHeaderContext.ADMIN_OR_APP_USER_JSON_STRING_HEADER_KEY);
         if (adminOrAppMemberJsonStringList != null && !adminOrAppMemberJsonStringList.isEmpty()) {
             return Mono.just(adminOrAppMemberJsonStringList.get(0))
+                    .subscribeOn(Schedulers.boundedElastic())
+
                     .flatMap(adminOrAppMemberJsonString ->
                             chain.filter(exchange)
                                     .contextWrite(context -> context.put(XHeaderContext.ADMIN_OR_APP_USER_JSON_STRING_HEADER_KEY, adminOrAppMemberJsonString))
-                    );
+                    )
+                    .publishOn(Schedulers.boundedElastic());
         }
-        return chain.filter(exchange);
+        return chain.filter(exchange)
+                .subscribeOn(Schedulers.boundedElastic())
+                .publishOn(Schedulers.boundedElastic());
     }
 }
