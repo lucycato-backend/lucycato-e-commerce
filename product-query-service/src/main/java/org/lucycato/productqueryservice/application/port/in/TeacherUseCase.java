56ec8668a2ade825bde4f7d1126671380d075ead
learnGrowD <dev_willd@naver.com>
2024-05-29 19:13:58 +0900

    feat/LUCY-027: teacher endpoint 정리

--- product-query-service/src/main/java/org/lucycato/productqueryservice/application/port/in/TeacherUseCase.java
+++ product-query-service/src/main/java/org/lucycato/productqueryservice/application/port/in/TeacherUseCase.java
@@ -1,10 +1,11 @@
 package org.lucycato.productqueryservice.application.port.in;
 
 
-import org.lucycato.productqueryservice.application.port.in.command.TeacherDetailSearchCommand;
-import org.lucycato.productqueryservice.application.port.in.command.TeacherSearchCommand;
+import org.lucycato.productqueryservice.application.port.in.command.*;
 import org.lucycato.productqueryservice.domain.Teacher;
+import org.lucycato.productqueryservice.domain.TeacherCourseSeries;
 import org.lucycato.productqueryservice.domain.TeacherDetail;
+import org.lucycato.productqueryservice.domain.TeacherNews;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -13,11 +14,11 @@ import java.util.List;
 public interface TeacherUseCase {
     Flux<Teacher> getTeachers(TeacherSearchCommand command);
 
+    Mono<TeacherDetail> getTeacher(TeacherDetailSearchCommand command);
+
     Flux<TeacherCourseSeries> getTeacherCourseSeries(TeacherCourseSeriesSearchCommand command);
 
     Flux<TeacherCourseSeries> getTeacherCourseSeries(SpecificTeacherCourseSeriesSearchCommand command);
-
-    Mono<TeacherDetail> getTeacher(TeacherDetailSearchCommand command);
 
     Flux<TeacherNews> getTeacherNews(TeacherNewsSearchCommand command);
 
