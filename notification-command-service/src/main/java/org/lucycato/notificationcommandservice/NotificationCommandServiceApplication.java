ffc465aa5c91b0fbf504f653460294ecf7e7aa3a
learnGrowD <dev_willd@naver.com>
2024-06-01 17:40:31 +0900

    feat/LUCY-main: infra conflict solve

--- notification-command-service/src/main/java/org/lucycato/notificationcommandservice/NotificationCommandServiceApplication.java
+++ notification-command-service/src/main/java/org/lucycato/notificationcommandservice/NotificationCommandServiceApplication.java
@@ -1,9 +1,11 @@
 package org.lucycato.notificationcommandservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan({"org.lucycato.common","org.lucycato.mvc"})
+@SpringBootApplication
 public class NotificationCommandServiceApplication {
 
     public static void main(String[] args) {
--- order-query-service/src/main/java/org/lucycato/orderqueryservice/OrderQueryServiceApplication.java
+++ order-query-service/src/main/java/org/lucycato/orderqueryservice/OrderQueryServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
 @ComponentScan({"org.lucycato.common", "org.lucycato.webflux"})
+@SpringBootApplication
 public class OrderQueryServiceApplication {
 
     public static void main(String[] args) {
