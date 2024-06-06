290b2583696d0ae70a69c38fd661edb094c851c4
learnGrowD <dev_willd@naver.com>
2024-06-06 18:22:00 +0900

    feat/LUCY-036: load balancer test add

--- event-command-service/src/main/java/org/lucycato/eventcommandservice/EventCommandServiceApplication.java
+++ event-command-service/src/main/java/org/lucycato/eventcommandservice/EventCommandServiceApplication.java
@@ -1,14 +1,34 @@
 package org.lucycato.eventcommandservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
+@RestController
 @SpringBootApplication
 public class EventCommandServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EventCommandServiceApplication.class, args);
+    }
+
+    @GetMapping("open-api/event-command/v1/test")
+    public Test hello() {
+        return new Test("Hello", "Good");
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Test {
+        private String name;
+
+        private String des;
     }
 }
--- event-query-service/src/main/java/org/lucycato/eventqueryservice/EventQueryServiceApplication.java
+++ event-query-service/src/main/java/org/lucycato/eventqueryservice/EventQueryServiceApplication.java
@@ -1,14 +1,44 @@
 package org.lucycato.eventqueryservice;
 
+import com.google.common.eventbus.AllowConcurrentEvents;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.lucycato.webflux.CommonWebClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @EnableDiscoveryClient
+@RestController
 @SpringBootApplication
 public class EventQueryServiceApplication {
 
+    private final CommonWebClient commonWebClient;
+
+    public EventQueryServiceApplication(CommonWebClient commonWebClient) {
+        this.commonWebClient = commonWebClient;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EventQueryServiceApplication.class, args);
+    }
+
+    @GetMapping("open-api/event-query/v1/test")
+    public Mono<Test> hello() {
+        String url = "lb://event-command-service/open-api/event-command/v1/test";
+        return commonWebClient.sendGetRequestResultMono(url, Test.class);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Test {
+        private String name;
+
+        private String des;
     }
 }
