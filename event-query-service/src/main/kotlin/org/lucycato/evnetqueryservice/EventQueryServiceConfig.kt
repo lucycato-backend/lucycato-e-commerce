887d1b32c8d569d1c9765f0b454d8147887bdc7d
learnGrowD <dev_willd@naver.com>
2024-06-13 23:09:38 +0900

    feat/LUCY-043: event delete

--- event-query-service/src/main/kotlin/org/lucycato/evnetqueryservice/EventQueryServiceConfig.kt
+++
@@ -1,9 +0,0 @@
-package org.lucycato.evnetqueryservice
-
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@ComponentScan("org.lucycato.common", "org.lucycato.webflux")
-class EventQueryServiceConfig {
-}
