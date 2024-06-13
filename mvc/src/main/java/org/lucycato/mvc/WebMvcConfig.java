3586b3e8282c45e7964ec5b1829d574238dc405b
witwint <witwint1234@gmail.com>
2024-06-13 15:34:49 +0900

    feat/LUCY-main: swagger only gateway

--- mvc/src/main/java/org/lucycato/mvc/WebMvcConfig.java
+++ mvc/src/main/java/org/lucycato/mvc/WebMvcConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -41,6 +42,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:9090","http://gateway-server:8080","/swagger-ui/**","/api-docs/**");
     }
 
 }
--- webflux/src/main/java/org/lucycato/webflux/WebFluxConfig.java
+++ webflux/src/main/java/org/lucycato/webflux/WebFluxConfig.java
@@ -6,6 +6,7 @@ import org.lucycato.webflux.resolver.WebFluxAppMemberMethodArgumentResolver;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
@@ -28,4 +29,11 @@ public class WebFluxConfig implements WebFluxConfigurer {
     public WebClient.Builder webClient() {
         return WebClient.builder();
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:9090","http://gateway-server:8080","/swagger-ui/**","/api-docs/**");
+    }
+
 }
