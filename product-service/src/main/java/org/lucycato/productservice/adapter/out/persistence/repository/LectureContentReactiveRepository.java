05129e9387848d4917bffd23f017cea6dff75fab
learnGrowD <dev_willd@naver.com>
2024-06-16 23:43:10 +0900

    feat/LUCY-043: file clean

--- product-service/src/main/java/org/lucycato/productservice/adapter/out/persistence/repository/LectureContentReactiveRepository.java
+++
@@ -1,8 +0,0 @@
-package org.lucycato.productservice.adapter.out.persistence.repository;
-
-import org.lucycato.productservice.adapter.out.persistence.entity.LectureContentR2dbcEntity;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface LectureContentReactiveRepository extends ReactiveCrudRepository<LectureContentR2dbcEntity, Long> {
-}
-
